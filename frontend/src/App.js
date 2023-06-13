2a7134a2e6ba010640cbb0b2ab9d6573aba402f2
Mogan <106959238+Mrdotov@users.noreply.github.com>
2023-06-13 18:17:48 +0300

    Will be added floder components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,6 @@
 import React, { useState, useEffect, createElement } from 'react';
 import appStyle from './App.css'
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-
-  const handleKeyDown = (event) => {
-    if (event.keyCode === 13) {
-      addTodo();
-    }
-  };
-
-  const addTodo = () => {
-    if (inputValue.trim() !== '') {
-      setTodos([...todos, inputValue]);
-      setInputValue('');
-    }
-  };
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-const DeleteTodo=(todo)=>{
-const updateTodos = todos.filter((item)=>item !== todo)
-  setTodos(updateTodos)
-
-}
-
-
-  return (
-    <div className='Todos'>
-        <div className='block_todos'>
-          <p>Todo</p>
-          <input
-            placeholder="Enter..."
-            value={inputValue}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-          />
-          <button onClick={addTodo}>Add</button>
-          <ul>
-            {todos.map((todo, index) => (
-              <li key={index}>{todo} <button onClick={()=>DeleteTodo(todo)}>Delete</button></li>
-            ))
-          
-            }
-          </ul>
-      </div>
-    </div>
-  );
-};
+import TodoList from './components/TodoList';
 
 function App(){
   return <TodoList />;
---
+++ frontend/src/components/TodoList.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+const TodoList = () => {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+  
+    const handleKeyDown = (event) => {
+      if (event.keyCode === 13) {
+        addTodo();
+      }
+    };
+  
+    const addTodo = () => {
+      if (inputValue.trim() !== '') {
+        setTodos([...todos, inputValue]);
+        setInputValue('');
+      }
+    };
+  
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value);
+    };
+  
+  const DeleteTodo=(todo)=>{
+  const updateTodos = todos.filter((item)=>item !== todo)
+    setTodos(updateTodos)
+  
+  }
+  
+  
+    return (
+      <div className='Todos'>
+          <div className='block_todos'>
+            <p>Todo</p>
+            <input
+              placeholder="Enter..."
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+              {todos.map((todo, index) => (
+                <li key={index}>{todo} <button onClick={()=>DeleteTodo(todo)}>Delete</button></li>
+              ))
+            
+              }
+            </ul>
+        </div>
+      </div>
+    );
+  };
+
+  export default TodoList;
